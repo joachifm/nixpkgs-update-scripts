b7a71682561055043391b7a32f008c7fb085e697
Joachim Fasting <joachifm@fastmail.fm>
2017-01-30 21:29:09 +0100

    src-info.nix: regen

--- src-info.nix
+++ src-info.nix
@@ -2,5 +2,4 @@
 // (import ./dnscrypt-proxy/src-info.nix)
 // (import ./electrum/src-info.nix)
 // (import ./grsecurity/src-info.nix)
-// (import ./tor/src-info.nix)
 // (import ./torbrowser/src-info.nix)
