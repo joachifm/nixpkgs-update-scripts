9f8478d40b4d47152ea290e316a8c6fedcf1b3c7
Joachim Fasting <joachifm@fastmail.fm>
2017-01-30 21:50:58 +0100

    electrum: point free refactor

--- electrum/update.bash
+++ electrum/update.bash
@@ -1,27 +1,25 @@
 #! /usr/bin/env bash
 
 sig_fprint+=("6694 D8DE 7BE8 EE56 31BE D950 2BD5 824B 7F94 70E6")
+download_page_url=https://download.electrum.org
 
-{
-page=index.html
-curl ${CURLOPTS[*]} -o "$page" --list-only -- https://download.electrum.org
-
-version=$(grep -Po '<a href="\K([[:digit:]]+\.?)+' $page | sort -Vu | tail -n1)
+version=$(curl ${CURLOPTS[*]} --list-only -- "$download_page_url" \
+    | grep -Po '<a href="\K([[:digit:]]+\.?)+' \
+    | sort -Vu \
+    | tail -n1)
 
 src_name=Electrum-$version
 src_file=$src_name.tar.gz
-src_url=https://download.electrum.org/$version/$src_file
+src_url=$download_page_url/$version/$src_file
 sig_url=$src_url.asc
 sig_file=$src_file.asc
 
-fetchurl "$src_url" "$src_file"
-fetchurl "$sig_url" "$sig_file"
+fetchurl "$src_url" "$src_file" >/dev/null
+fetchurl "$sig_url" "$sig_file" >/dev/null
 
 pgp_recvkeys "${sig_fprint[@]}"
 pgp_verifysig "$sig_file" "$src_file"
-
 src_sha512=$(sha512file "$src_file")
-} >&2
 
 cat <<EOF
 { fetchurl }:
