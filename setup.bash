4372bd0c6c4b18eb1037efb0ec44adc212f2705f
Joachim Fasting <joachifm@fastmail.fm>
2017-01-30 18:29:32 +0100

    subr.bash (fetchurl): init

--- setup.bash
+++ setup.bash
@@ -11,3 +11,14 @@ export LC_ALL LC_COLLATE LC_MESSAGES
 HOME=$PWD
 export GNUPGHOME=$PWD/.gnupg
 [[ -d "$GNUPGHOME" ]] || gpg --batch --list-keys &>/dev/null
+
+if [[ -d /etc/ssl/certs ]] ; then
+    capath=/etc/ssl/certs
+else
+    echo "unable to determine CA path" >&2
+    exit 1
+fi
+
+CURLOPTS+=("--capath $capath" "--cert-status")
+CURLOPTS+=("-q" "-f" "-s" "-L")
+export CURLOPTS
--- subr.bash
+++ subr.bash
@@ -1,9 +1,36 @@
 #! /usr/bin/env bash
 
 fetchurl() {
-    local url=${1?URL} name=${2:-${url##*/}}
-    wget --continue -O "$name" -- "$url"
-    echo "$name"
+    local url=${1?URL}
+    local name=${2:-${url##*/}}
+
+    local part=$name.part
+    local opts=()
+
+    # exit early if destination exists
+    if [[ -s "$name" ]] ; then
+        echo "$name"
+        return
+    fi
+
+    # decide whether to resume partial transfer
+    if [[ ! -e "$name" ]] && [[ -e "$part" ]] ; then
+        opts+=("-C -")
+    fi
+
+    # begin transfer
+    curl ${CURLOPTS[*]} ${opts[*]} -o "$part" -- "$url"
+    ret=$?
+
+    # process return code
+    case "$ret" in
+        0)
+            mv "$part" "$name"
+            echo "$name"
+            ;;
+        *)
+            return $ret
+    esac
 }
 
 sha512file() {
