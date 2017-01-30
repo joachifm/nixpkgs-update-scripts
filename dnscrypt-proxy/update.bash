1c856c8416dbd781afb5967daddae9db0330d82a
Joachim Fasting <joachifm@fastmail.fm>
2017-01-30 21:25:02 +0100

    dnscrypt-proxy: curl only

--- dnscrypt-proxy/update.bash
+++ dnscrypt-proxy/update.bash
@@ -4,19 +4,18 @@ sig_pubkey="RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
 
 {
 page=index.html
-test -f "$page" || {
-    curl -L -s --list-only -- https://download.dnscrypt.org/dnscrypt-proxy &>$page
-}
+curl ${CURLOPTS[*]} --list-only -o "$page" -- https://download.dnscrypt.org/dnscrypt-proxy
+
 src_file=$(grep -Eo 'dnscrypt-proxy-[[:digit:]]\.[[:digit:]]\.[[:digit:]]\.tar\.bz2' "$page" | sort -Vu | tail -n1)
 src_url=https://download.dnscrypt.org/dnscrypt-proxy/$src_file
 src_name=${src_file/.tar.bz2/}
 src_vers=(${src_name//-/ })
 version=${src_vers[2]}
-wget --continue -O "$src_file" -- "$src_url"
+fetchurl "$src_url" "$src_file" >/dev/null
 
 sig_url=$src_url.minisig
 sig_file=${sig_url##*/}
-wget --continue -O "$sig_file" -- "$sig_url"
+fetchurl "$sig_url" "$sig_file" >/dev/null
 
 minisign -VP "$sig_pubkey" -x "$sig_file" -m "$src_file"
 src_sha512=$(sha512file "$src_file")
