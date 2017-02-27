2d454ff28e61695d03ed6732865a97cf051fee9a
Joachim Fasting <joachifm@fastmail.fm>
2017-02-27 16:02:08 +0100

    grsecurity: update

--- grsecurity/src-info.nix
+++ grsecurity/src-info.nix
@@ -1,24 +1,24 @@
 {
   grsecurity-testing-patch = {
     src = fetchurl {
-      url = https://grsecurity.net/test/grsecurity-3.1-4.9.12-201702231830.patch;
-      sha512 = "1455wpdy5lizdibs86llaxf087vggl921dn83pf4cnc9fbzc5mwa46vazahv9g8467bb335vgg5fcba2i0026s1h9vcsvyy6470m4i3";
+      url = https://grsecurity.net/test/grsecurity-3.1-4.9.13-201702270729.patch;
+      sha512 = "07sbgvksjrvawisq4knjq34022blgg8f14blv22d37siplyamydarsykai5dks5573k61dv9q027jigxyamy50h0fd5ic8qpdp0y30c";
     };
     meta = {
       grver = "3.1";
-      grrev = "201702231830";
-      kver = "4.9.12";
-      version = "3.1-4.9.12-201702231830";
+      grrev = "201702270729";
+      kver = "4.9.13";
+      version = "3.1-4.9.13-201702270729";
     };
   };
 
   linux-grsecurity = {
     src = fetchurl {
-      url = https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.9.12.tar.xz;
-      sha512 = "37riipfdm8xw53598w15l3x0gy06rmp22j1yf5wql2d1fzhgf29x007axdi7swq4jnpihd934mvpfb1sy42srkinps0x5gvv65n00bs";
+      url = https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.9.13.tar.xz;
+      sha512 = "2mszr7n212ryaw5q2lj92766aqnjznkfr7ly04g2ggzk7cyiirrf1w37d9arzg3smn5695fb1frikacnbrmz2hsaia48kjhwgjbxn2h";
     };
     meta = {
-      version = "4.9.12";
+      version = "4.9.13";
       branch = "4.9";
     };
   };
