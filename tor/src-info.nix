9b5acec080500782024ba2a8d3b37b74bad852e7
Joachim Fasting <joachifm@fastmail.fm>
2017-03-02 12:51:19 +0100

    tor: update

--- tor/src-info.nix
+++ tor/src-info.nix
@@ -1,11 +1,12 @@
 {
   tor = {
     src = fetchurl {
-      url = https://dist.torproject.org/tor-0.2.9.9.tar.gz;
-      sha512 = "2dqr4inf0xah3ylg0ivzsnyx234lbghlzmk7692h4dyir3cbrkdwhd2qfm2nbsyzvn5mqb06qnc7l93rx3p2swia3qlbf83wpry3ryb";
+      url = https://dist.torproject.org/tor-0.2.9.10.tar.gz;
+      sha512 = "3hvfksxr76j2xmp8a45hlnnywl8nxfpi9nvv8crxkm4d80l7ap9gf3i3m29jyjymgdfl2vp08znz2f2dc7dgw1na4b08vs032plz361";
+      sha256 = "0h8kpn42mgpkzmnga143hi8nh0ai65ypxh7qhkwbb15j3wz2h4fn";
     };
     meta = {
-      version = "0.2.9.9";
+      version = "0.2.9.10";
     };
   };
 }
