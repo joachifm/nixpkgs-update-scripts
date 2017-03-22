fc7b25c37e29ea25d80d4c67e1df7e303f0d0861
Joachim Fasting <joachifm@fastmail.fm>
2017-03-23 00:58:02 +0100

    electrum: update

--- electrum/src-info.nix
+++ electrum/src-info.nix
@@ -1,12 +1,12 @@
 {
   electrum = {
     src = fetchurl {
-      url = https://download.electrum.org/2.8.1/Electrum-2.8.1.tar.gz;
-      sha512 = "2fvv5vn99fxlwwracsq6z0jrf2iy59jc5kkmwlfiacxrb5khr4hs4kpvab7md5ckgmr9hb0hi6icg75dwrw78fwqmi5c2287xs8ppax";
-      sha256 = "1398s9d8j04is24il2xjb6xkj666pj21bsr90xglpsmfa1js9z7g";
+      url = https://download.electrum.org/2.8.2/Electrum-2.8.2.tar.gz;
+      sha512 = "2fwyzk2akdg3af1nxsxfm1b2fvkf5dy431nfanx8h9d63rj7fswb0za77z0m84sz6b64r1zh7a6j0aff3ll1n0iwcyyzd712yqd8kag";
+      sha256 = "01xphbi7lx64s9380zjfakz5h8blqmxp0ryqlll7px66qpmjn5fq";
     };
     meta = {
-      version = "2.8.1";
+      version = "2.8.2";
     };
   };
 }
