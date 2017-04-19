ba7d86229587b4eddeeb5849c793cc1fb09c36c0
Joachim Fasting <joachifm@fastmail.fm>
2017-04-19 23:56:35 +0200

    torbrowser: update

--- torbrowser/src-info.nix
+++ torbrowser/src-info.nix
@@ -1,24 +1,24 @@
 {
   tor-browser-en-US-linux32 = {
     src = fetchurl {
-      url = https://dist.torproject.org/torbrowser/6.5.1/tor-browser-linux32-6.5.1_en-US.tar.xz;
-      sha512 = "3da2425hib3b45c77i6xhn83zsd87jvcqxfap6cxsyfh7scfr3pm6gl6z5hs82sxl3bwf94iwdxm7m926dzkf2ffrfnafgnrpgmzk6m";
-      sha256 = "1zfghr01bhpn39wqaw7hyx7yap7xyla4m3mrgz2vi9a5qsyxmbcr";
+      url = https://dist.torproject.org/torbrowser/6.5.2/tor-browser-linux32-6.5.2_en-US.tar.xz;
+      sha512 = "1bpsarb1fy54i01q84k2c8sq05cdr0hb2c3jaalz5qpifsyw4p1y1ji3i09qd2zba7mmmn2a5sy35qv6hkaibif2jf62w93pplj33n0";
+      sha256 = "0micxgkbys0py4bj6csbc8xz4gq0x5v2zirgi38krnm5x5riqj3w";
     };
     meta = {
-      version = "6.5.1";
+      version = "6.5.2";
       platform = "linux32";
       lang = "en-US";
     };
   };
   tor-browser-en-US-linux64 = {
     src = fetchurl {
-      url = https://dist.torproject.org/torbrowser/6.5.1/tor-browser-linux64-6.5.1_en-US.tar.xz;
-      sha512 = "0n4nlskxngxpcm5hm5m6xgl96q5ngyx01g9jsk9gja70ci46yhk818pjwwnmnn8asrg70ksqdw7hmqym6gbs0a5qi0qg2mc5innzqij";
-      sha256 = "1p2bgavvyzahqpjg9vp14c0s50rmha3v1hs1c8zvz6fj8fgrhn0i";
+      url = https://dist.torproject.org/torbrowser/6.5.2/tor-browser-linux64-6.5.2_en-US.tar.xz;
+      sha512 = "1f1wx9y06awyz7m9mfyaq9qyjiwbmcbs0ablljdr7acbzchzha6nlgzp07qcm4pr79fh9v5iibw7ryg2c1yhsrwaahd0yq54wf7brjq";
+      sha256 = "0jn98arczlgjigpmql1qg5b7izabv4zy4mji6vvcg3b8g1ma108r";
     };
     meta = {
-      version = "6.5.1";
+      version = "6.5.2";
       platform = "linux64";
       lang = "en-US";
     };
